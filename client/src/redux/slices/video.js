39555d69a72d41705e8bcbb11d767735a924d8b2
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2022-09-08 22:10:02 +0200

    Create `video` redux store slice
    - Create `video` redux store slice.
    - Create extraReducers to update `video` state based on state changes
      from `colyseus` middleware.

---
+++ client/src/redux/slices/video.js
@@ -0,0 +1,42 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+import { video } from '../middlewares/colyseus/actions';
+
+const initialState = {
+  player: {
+    url: '',
+    subtitles: '',
+    progress: 0,
+    playing: false,
+    updatedAt: 0,
+  },
+  requests: {
+    progress: false,
+  },
+};
+
+const slice = createSlice({
+  name: 'video',
+  initialState: initialState,
+  extraReducers: (builder) => {
+    builder.addCase(video.progress.response, (state) => {
+      state.requests.progress = false;
+    });
+
+    builder.addCase(video.progress.onRequest, (state) => {
+      state.requests.progress = true;
+    });
+
+    builder.addCase(video.progress.onResponse, (state, action) => {
+      state.player.progress = action.payload.progress;
+    });
+
+    builder.addCase(video.onStateChanges, (state, action) => {
+      for (const change of action.payload.changes) {
+        state.player[change.field] = change.value;
+      }
+    });
+  },
+});
+
+export default slice.reducer;
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import room from './slices/room';
+import video from './slices/video';
 
 import colyseus from './middlewares/colyseus';
 
 export const store = configureStore({
   reducer: {
     room: room,
+    video: video,
   },
   middleware: [colyseus],
 });
