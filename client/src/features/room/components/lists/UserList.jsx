4ff78be9f85e05a5cbe4de96f0bcdc1f30f88988
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2022-09-11 00:46:08 +0200

    Create `UserList` component
    - Create `UserList` and `UserListItem` components that will be used to
      display the list of users in the current room.
    - Both components are using `forwardRef` to pass the `ref` to the
      container element.
    - Export `UserList` component as named export from `room` feature.

---
+++ client/src/features/room/components/lists/UserList.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import { forwardRef } from 'react';
+
+import { Paper, AvatarGroup } from '@mui/material';
+
+import UserListItem from './UserListItem';
+
+const UserList = forwardRef(({ users }, ref) => {
+  return (
+    <Paper component={AvatarGroup} max={8} sx={{ p: 2, justifyContent: 'center' }} ref={ref}>
+      {users.map((user) => (
+        <UserListItem key={user.id} user={user} />
+      ))}
+    </Paper>
+  );
+});
+
+UserList.displayName = 'UserList';
+
+UserList.propTypes = {
+  users: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+export default UserList;
--- client/src/features/room/index.js
+++ client/src/features/room/index.js
@@ -1,2 +1,4 @@
 export { default as CreateRoomForm } from './components/forms/CreateRoomForm';
 export { default as JoinRoomForm } from './components/forms/JoinRoomForm';
+
+export { default as UserList } from './components/lists/UserList';
