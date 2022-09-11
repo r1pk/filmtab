d61072778d4af669de2b426f3b298d28deeb59ed
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2022-09-11 20:14:02 +0200

    Create `LeaveRoomButton` component
    - Create `LeaveRoomButton` component that will allow user to leave
      the room.

--- client/src/features/room/index.js
+++ client/src/features/room/index.js
@@ -1,3 +1,5 @@
+export { default as LeaveRoomButton } from './components/buttons/LeaveRoomButton';
+
 export { default as CreateRoomForm } from './components/forms/CreateRoomForm';
 export { default as JoinRoomForm } from './components/forms/JoinRoomForm';
 
