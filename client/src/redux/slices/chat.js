eb878115e715063e362b86ba2db221a282d711c2
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2022-09-23 22:55:06 +0200

    Add chat notifications when user joins or leaves
    - Add simple chat message when user joins or leaves room.

--- client/src/redux/slices/chat.js
+++ client/src/redux/slices/chat.js
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+import { nanoid } from 'nanoid';
 import * as colyseus from '../middlewares/colyseus';
 
 const initialState = {
@@ -21,6 +22,32 @@ const slice = createSlice({
       }
 
       state.messages.push(action.payload.message);
+    });
+
+    builder.addCase(colyseus.actions.userJoined.type, (state, action) => {
+      state.messages.push({
+        id: nanoid(),
+        content: `${action.payload.user.username} joined the room.`,
+        createdAt: new Date().getTime(),
+        author: {
+          id: nanoid(),
+          username: 'FilmTab',
+          color: 'primary.main',
+        },
+      });
+    });
+
+    builder.addCase(colyseus.actions.userLeft.type, (state, action) => {
+      state.messages.push({
+        id: nanoid(),
+        content: `${action.payload.user.username} left the room.`,
+        createdAt: new Date().getTime(),
+        author: {
+          id: nanoid(),
+          username: 'FilmTab',
+          color: 'primary.main',
+        },
+      });
     });
 
     builder.addCase(colyseus.actions.leaveRoom.type, () => initialState);
