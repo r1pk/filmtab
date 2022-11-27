a908fd6544c3a39248c042c10f7c3e8792ee4d78
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2022-11-27 05:04:19 +0100

    Change project file structure
    - Move code from `AppProvider` component to main `App` component.
    - Remove `providers` folder.
    - Rename `AppRoutes` component to `Routes`.
    - Update project structure in `README.md`.
    - Update project version.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,27 @@
-import { AppProvider } from '@/providers';
+import { Provider } from 'react-redux';
+import { store } from '@/redux';
+
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { theme } from '@/theme';
+
+import { BrowserRouter } from 'react-router-dom';
+import { Routes } from '@/routes';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-  return <AppProvider />;
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+        <ToastContainer theme="dark" position={toast.POSITION.BOTTOM_LEFT} />
+      </ThemeProvider>
+    </Provider>
+  );
 };
 
 export default App;
