467008e2c5012b46f0eb2e1710ffbe1a700f0244
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2023-01-21 20:01:16 +0100

    Rewrite `UsernameManager` to use simple functions
    - Rewrite `UsernameManager` to use simple functions instead of a class.

--- client/src/features/room/utils/usernameManager.js
+++ client/src/features/room/utils/usernameManager.js
@@ -1,38 +1,43 @@
 import { uniqueNamesGenerator, colors, animals } from 'unique-names-generator';
 
-class UsernameManager {
-  saveUsername(username) {
-    return window.localStorage.setItem(import.meta.env.VITE_USERNAME_LOCALSTORAGE_KEY, username);
+const saveUsername = (username) => {
+  return window.localStorage.setItem(import.meta.env.VITE_USERNAME_LOCALSTORAGE_KEY, username);
+};
+
+const getSavedUsername = () => {
+  return window.localStorage.getItem(import.meta.env.VITE_USERNAME_LOCALSTORAGE_KEY);
+};
+
+const getRandomUsername = () => {
+  return uniqueNamesGenerator({
+    style: 'capital',
+    separator: '',
+    dictionaries: [colors, animals],
+    length: 2,
+  });
+};
+
+const removeSavedUsername = () => {
+  return window.localStorage.removeItem(import.meta.env.VITE_USERNAME_LOCALSTORAGE_KEY);
+};
+
+const isUsernameAlreadySaved = () => {
+  return Boolean(getSavedUsername());
+};
+
+const getRecommendedUsername = () => {
+  if (isUsernameAlreadySaved()) {
+    return getSavedUsername();
   }
 
-  getSavedUsername() {
-    return window.localStorage.getItem(import.meta.env.VITE_USERNAME_LOCALSTORAGE_KEY);
-  }
+  return getRandomUsername();
+};
 
-  removeSavedUsername() {
-    return window.localStorage.removeItem(import.meta.env.VITE_USERNAME_LOCALSTORAGE_KEY);
-  }
-
-  isUsernameAlreadySaved() {
-    return Boolean(this.getSavedUsername());
-  }
-
-  generateRandomUsername() {
-    return uniqueNamesGenerator({
-      style: 'capital',
-      separator: '',
-      dictionaries: [colors, animals],
-      length: 2,
-    });
-  }
-
-  getRecommendedUsername() {
-    if (this.isUsernameAlreadySaved()) {
-      return this.getSavedUsername();
-    }
-
-    return this.generateRandomUsername();
-  }
-}
-
-export const usernameManager = new UsernameManager();
+export const usernameManager = {
+  saveUsername,
+  getSavedUsername,
+  getRandomUsername,
+  removeSavedUsername,
+  isUsernameAlreadySaved,
+  getRecommendedUsername,
+};
