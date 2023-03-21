9cc8fdbbb5714556eaacd8ed0254827542eba7bc
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2023-03-21 14:51:14 +0100

    Move form schemas to the same file as component
    - Move form schemas to the same file as component. This is to make it
      easier to find the schema for a specific form.

--- client/src/features/room/schemas/CreateRoomFormSchema.js
+++
@@ -1,6 +0,0 @@
-import Joi from 'joi';
-
-export const CreateRoomFormSchema = Joi.object({
-  username: Joi.string().trim().alphanum().min(3).max(20).required().label('username'),
-  rememberUsername: Joi.boolean().label('rememberUsername'),
-});
--- client/src/features/video/schemas/SetVideoFormSchema.js
+++
@@ -1,5 +0,0 @@
-import Joi from 'joi';
-
-export const SetVideoFormSchema = Joi.object({
-  url: Joi.string().trim().uri().required().label('url'),
-});
