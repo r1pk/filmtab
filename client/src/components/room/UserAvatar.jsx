da9a6007b9534d3bca386c644e3f15f468393f4c
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2023-05-19 15:35:22 +0200

    Rename `UserChip` component to `UserAvatar`
    - Rename `UserChip` component to `UserAvatar` to better reflect its
      purpose.

--- client/src/components/room/UserAvatar.jsx
+++ client/src/components/room/UserAvatar.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 import { Avatar, Tooltip } from '@mui/material';
 
-const UserChip = forwardRef(({ user, ...rest }, ref) => {
+const UserAvatar = forwardRef(({ user, ...rest }, ref) => {
   return (
     <Tooltip title={user.username}>
       <Avatar name={user.username} alt={user.username} sx={{ backgroundColor: user.color }} ref={ref} {...rest}>
@@ -14,13 +14,13 @@ const UserChip = forwardRef(({ user, ...rest }, ref) => {
   );
 });
 
-UserChip.displayName = 'UserChip';
+UserAvatar.displayName = 'UserAvatar';
 
-UserChip.propTypes = {
+UserAvatar.propTypes = {
   user: PropTypes.shape({
     username: PropTypes.string.isRequired,
     color: PropTypes.string.isRequired,
   }).isRequired,
 };
 
-export default UserChip;
+export default UserAvatar;
--- client/src/components/room/UserList.jsx
+++ client/src/components/room/UserList.jsx
@@ -4,14 +4,14 @@ import PropTypes from 'prop-types';
 
 import { AvatarGroup, Paper, Skeleton } from '@mui/material';
 
-import UserChip from './UserChip';
+import UserAvatar from './UserAvatar';
 
 const UserList = forwardRef(({ users, ...rest }, ref) => {
   return (
     <Paper sx={{ p: 1 }} ref={ref} {...rest}>
       <AvatarGroup max={7} sx={{ justifyContent: 'center' }}>
         {users.length === 0 && <Skeleton variant="circular" width={44} height={44} />}
-        {users.length > 0 && users.map((user) => <UserChip key={user.id} user={user} />)}
+        {users.length > 0 && users.map((user) => <UserAvatar key={user.id} user={user} />)}
       </AvatarGroup>
     </Paper>
   );
