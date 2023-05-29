74a3d5d50d10e504b939a50ab712cf84eece87d0
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2023-05-29 21:11:17 +0200

    Group related code into one file
    - Move available video providers into file which is responsible for
      building player source (`build-player-source.js`). Main reason
      is to have all related code responsible for building player source
      in one place for easier maintenance and future improvements.
    - Export available video providers from `build-player-source.js`
      as `providers` array.

--- client/src/utils/build-player-source.js
+++ client/src/utils/build-player-source.js
@@ -1,19 +1,27 @@
-import { playerProviders } from '@/configs/player-providers';
+export const providers = [
+  { name: 'youtube', regexp: /((http(s)?:\/\/)?)(www\.)?((youtube\.com\/)|(youtu.be\/))[\S]+/ },
+  { name: 'vimeo', regexp: /vimeo\.com\/(?!progressive_redirect).+/ },
+  { name: 'html5', regexp: /\.(mp4|og[gv]|webm|mov|m4v)(#t=[,\d+]+)?($|\?)/i },
+  { name: 'html5', regexp: /^(https:\/\/www\.googleapis\.com\/drive\/v3\/files\/)/ },
+];
 
 export const buildPlayerSource = (url) => {
-  const provider = playerProviders.find((provider) => provider.regexp.test(url));
-
-  const source = {
+  const provider = providers.find((provider) => provider.regexp.test(url));
+  const defaultPlayerSource = {
     type: 'video',
     sources: [],
   };
 
   if (provider) {
-    source.sources.push({
-      src: url,
-      provider: provider.name,
+    return Object.assign({}, defaultPlayerSource, {
+      sources: [
+        {
+          src: url,
+          provider: provider.name,
+        },
+      ],
     });
   }
 
-  return source;
+  return defaultPlayerSource;
 };
